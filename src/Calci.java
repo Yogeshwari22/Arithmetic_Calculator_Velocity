25468482bfbfb10b418da409ea05ae6bfbf1d50e
ynpat <ynpatil2230@gmail.com>
2022-08-26 14:21:59 +0530

    operations done

---
+++ src/Calci.java
@@ -0,0 +1,63 @@
+import java.util.*;
+import java.util.Scanner;
+public class Calci {
+	 public static void main(String[] args)
+	    {
+	        System.out.println("Enter first and second number:");
+	        Scanner inp = new Scanner(System.in);
+	        int num1,num2;
+	        num1 = inp.nextInt();
+	        num2 = inp.nextInt();
+	        int ans;
+	        System.out.println("Enter your selection:\n" + "1.Addition\n"+ "2.Subtraction\n" +"3.Multiplication\n" +"4.Division:");
+	        int choose;
+	        choose = inp.nextInt();
+	        switch (choose){
+	        case 1:
+	            System.out.println(add( num1,num2));
+	            break;
+	        case 2:
+	            System.out.println(sub( num1,num2));
+	            break;      
+	        case 3:
+	            System.out.println(mult( num1,num2));
+	            break;
+	        case 4:
+	            System.out.println(div( num1,num2));
+	            break;
+	            default:
+	                System.out.println("Illigal Operation");
+
+
+	        }
+
+
+
+	    }
+	    public static int add(int x, int y)
+	    {
+	        int result = x + y;
+	        return result;
+	    }
+	    public static int sub(int x, int y)
+	    {
+	        int result = x-y;
+	        return result;
+	    }
+	    public static int mult(int x, int y)
+	    {
+	        int result = x*y;
+	        return result;
+	    }
+	    public static int div(int x, int y)
+	    {
+	        int result = x/y;
+	        return result;
+	    }
+
+
+}
+
+
+
+   
